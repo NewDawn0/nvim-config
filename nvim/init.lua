b39e85cf76d5ac2208cac192c696ef56bbd46179
NewDawn0 <newdawn.v0.0+git@gmail.com>
2025-01-10 00:10:46 +0100

    add toggle to conform

--- nvim/init.lua
+++ nvim/init.lua
@@ -8,41 +8,41 @@
 -- Author: NewDawn0 (https://github.com/NewDawn0)
 
 -- Load utility stuff
-require "util.startup"
-require "util"
+require("util.startup")
+require("util")
 
 -- Import plugin loader
 -- local loader = require "util.loader"
 local plugins = {
-  "plugins.alpha",
-  "plugins.boole",
-  "plugins.bufferline",
-  "plugins.cmp",
-  "plugins.colorizer",
-  "plugins.comment",
-  "plugins.conform",
-  "plugins.dap",
-  "plugins.flash",
-  "plugins.gen",
-  "plugins.gitsigns",
-  "plugins.indent",
-  "plugins.lint",
-  "plugins.lsp-zero",
-  "plugins.lualine",
-  "plugins.notify",
-  "plugins.nvim-autopairs",
-  "plugins.oil",
-  "plugins.surround",
-  "plugins.telescope",
-  "plugins.tmux",
-  "plugins.todo-comments",
-  "plugins.tokyonight",
-  "plugins.treesitter",
-  "plugins.trim",
-  "plugins.winsep",
-  "plugins.wrapping",
+	"plugins.alpha",
+	"plugins.boole",
+	"plugins.bufferline",
+	"plugins.cmp",
+	"plugins.colorizer",
+	"plugins.comment",
+	"plugins.conform",
+	"plugins.dap",
+	"plugins.flash",
+	"plugins.gen",
+	"plugins.gitsigns",
+	"plugins.indent",
+	"plugins.lint",
+	"plugins.lsp-zero",
+	"plugins.lualine",
+	"plugins.notify",
+	"plugins.nvim-autopairs",
+	"plugins.oil",
+	"plugins.surround",
+	"plugins.telescope",
+	"plugins.tmux",
+	"plugins.todo-comments",
+	"plugins.tokyonight",
+	"plugins.treesitter",
+	"plugins.trim",
+	"plugins.winsep",
+	"plugins.wrapping",
 }
 
 for _, path in ipairs(plugins) do
-  require(path)
+	require(path)
 end
--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -8,52 +8,75 @@
 -- Author: NewDawn0 (https://github.com/NewDawn0)
 
 -- Setup auto formatting
-require("conform").setup {
-  format_on_save = {
-    timeout_ms = 3000,
-    lsp_format = "fallback",
-  },
-  formatters = {
-    black = { prepend_args = { "--fast" } },
-    gleam = { prepend_args = { "format" } },
-    hydrogen = { command = "", stdin = false },
-    proto = { command = "buf", args = { "format", "$FILENAME" }, stdin = true },
-    taplo = { prepend_args = { "format", "$FILENAME" }, stdin = false },
-    zig = { command = "zig", args = { "fmt", "$FILENAME" }, stdin = false },
-  },
-  formatters_by_ft = {
-    asm = { "asmfmt" },
-    astro = { "prettierd" },
-    c = { "clang-format" },
-    cpp = { "clang-format" },
-    css = { "prettierd" },
-    fennel = { "fnlfmt" },
-    fortran = { "fprettify" },
-    gleam = { "gleam" },
-    go = { "gofmt" },
-    haskell = { "ormulu" },
-    html = { "prettierd" },
-    hydrogen = { "hydrogen" },
-    java = { "google-java-format" },
-    javascript = { "prettierd" },
-    javascriptreact = { "prettierd" },
-    json = { "prettierd" },
-    lua = { "stylua" },
-    markdown = { "prettierd" },
-    nix = { "nixfmt" },
-    ocaml = { "ocamlformat" },
-    perl = { "perltidy" },
-    proto = { "proto" },
-    python = { "isort", "black" },
-    rust = { "rustfmt" },
-    sh = { "beautysh", "shellharden" },
-    svelte = { "prettierd" },
-    toml = { "taplo" },
-    typescript = { "prettierd" },
-    typescriptreact = { "prettierd" },
-    vue = { "prettierd" },
-    yaml = { "yamlfmt" },
-    zig = { "zig" },
-    zsh = { "beautysh", "shellharden" },
-  },
-}
+require("conform").setup({
+	format_on_save = function(bufnr)
+		-- Disable with a global or buffer-local variable
+		if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+			return
+		end
+		return { timeout_ms = 3000, lsp_format = "fallback" }
+	end,
+	formatters = {
+		black = { prepend_args = { "--fast" } },
+		gleam = { prepend_args = { "format" } },
+		hydrogen = { command = "", stdin = false },
+		proto = { command = "buf", args = { "format", "$FILENAME" }, stdin = true },
+		taplo = { prepend_args = { "format", "$FILENAME" }, stdin = false },
+		zig = { command = "zig", args = { "fmt", "$FILENAME" }, stdin = false },
+	},
+	formatters_by_ft = {
+		asm = { "asmfmt" },
+		astro = { "prettierd" },
+		c = { "clang-format" },
+		cpp = { "clang-format" },
+		css = { "prettierd" },
+		fennel = { "fnlfmt" },
+		fortran = { "fprettify" },
+		gleam = { "gleam" },
+		go = { "gofmt" },
+		haskell = { "ormulu" },
+		html = { "prettierd" },
+		hydrogen = { "hydrogen" },
+		java = { "google-java-format" },
+		javascript = { "prettierd" },
+		javascriptreact = { "prettierd" },
+		json = { "prettierd" },
+		lua = { "stylua" },
+		markdown = { "prettierd" },
+		nix = { "nixfmt" },
+		ocaml = { "ocamlformat" },
+		perl = { "perltidy" },
+		proto = { "proto" },
+		python = { "isort", "black" },
+		rust = { "rustfmt" },
+		sh = { "beautysh", "shellharden" },
+		svelte = { "prettierd" },
+		toml = { "taplo" },
+		typescript = { "prettierd" },
+		typescriptreact = { "prettierd" },
+		vue = { "prettierd" },
+		yaml = { "yamlfmt" },
+		zig = { "zig" },
+		zsh = { "beautysh", "shellharden" },
+	},
+})
+
+vim.api.nvim_create_user_command("FormatDisable", function(args)
+	vim.notify("Formatting disabled")
+	if args.bang then
+		-- FormatDisable! will disable formatting just for this buffer
+		vim.b.disable_autoformat = true
+	else
+		vim.g.disable_autoformat = true
+	end
+end, {
+	desc = "Disable autoformat-on-save",
+	bang = true,
+})
+vim.api.nvim_create_user_command("FormatEnable", function()
+	vim.notify("Enabled")
+	vim.b.disable_autoformat = false
+	vim.g.disable_autoformat = false
+end, {
+	desc = "Re-enable autoformat-on-save",
+})
